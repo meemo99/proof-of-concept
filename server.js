af898465101e635dad1be2eba384bcbf6f9eed56
meemo99 <oumaima.el.fakir@hva.nl>
2026-06-05 09:51:54 +0200

    routes klaar gezet
    #8

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
 console.log('Hier komt je server voor Sprint 12.')
+app.get('/dashboard/:city', async function (req, res) {
+    const apiResponse = await fetch('https://fdnd-agency.directus.app/items/ctc_smartzone/?filter[city][_eq]=${req.params.city}')
+    const apiResponseJSON = await apiResponse.json()
+    res.render('dashboard.liquid', { city: apiResponseJSON.data })
+})
